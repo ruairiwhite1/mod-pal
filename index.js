422823e97f77879b8771af806179b69f0e39d6f9
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-01-15 18:50:08 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js')
-const client = new Discord.client()
+const client = new Discord.Client()
 const config = require('./config.json')
 
 client.on('ready', () => {
     console.log('Mod Pal online!')
 })
+
+client.login(config.token)
