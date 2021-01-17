4a89093d323c5bc8f747b149783a9207fee849a9
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-01-17 11:54:43 +0000

    update

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('@root/config.json')
 const mongo = require('@util/mongo')
+const loadCommands = require('./commands/load-commands')
+const command = require('@util/command')
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log('Mod Pal online!')
+
+    loadCommands(client)
+
+    await mongo().then(mongoose => {
+        try {
+            console.log('Connected to Mongo!')
+        } finally {
+          mongoose.connection.close()
+        }
+    })
 })
 
 client.login(config.token)
