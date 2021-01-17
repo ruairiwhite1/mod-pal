f83b555834bc9d68669d55135704121b2e74e7a2
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-01-17 11:29:32 +0000

    Create mongo.js

---
+++ util/mongo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+// const { mongoPath } = require('./config.json')
+
+const mongoPath =
+  'mongodb+srv://ModPalOwner:Celtic62@mod-pal-bot.locol.mongodb.net/ModPal?retryWrites=true&w=majority'
+module.exports = async () => {
+  await mongoose.connect(mongoPath, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  return mongoose
+}
