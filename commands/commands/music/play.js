921bdd13d719001e51f807e8a19a320c7ae36655
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-05-10 14:34:16 +0100

    Added music commands

---
+++ commands/commands/music/play.js
@@ -0,0 +1,13 @@
+const distube = require('distube')
+
+module.exports = {
+    commands: ['play', 'p'],
+    maxArgs: 1,
+    expectedArgs: "[]",
+    description: 'Displays a users balance',
+    callback: async (message, args) => {
+        const music = args.join(" ");
+        
+        bot.distube.play(message, music)
+    }
+}
--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const command = require('@util/command')
 const loadFeatures = require('@root/features/load-features')
 const { DiscordUNO } = require("discord-uno");
 const discordUNO = new DiscordUNO();
+const DisTube = require('distube')
 
+bot.distube = new DisTube(bot, {searchSongs: false, emitNewSongOnly: true})
 
 client.on('ready', async () => {
     console.log('Mod Pal online!')
