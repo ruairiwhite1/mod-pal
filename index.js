5aa5e9203148aa65ca68edd6ffc8bc5d3b147a78
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-05-10 11:13:35 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const client = new Discord.Client()
 
 //const config = require('@root/config.json')
 const mongo = require('@util/mongo')
-const loadCommands = require('./commands/load-commands')
+const loadCommands = require('@root/commands/load-commands')
 const command = require('@util/command')
 const loadFeatures = require('@root/features/load-features')
 const { DiscordUNO } = require("discord-uno");
@@ -43,81 +43,5 @@ client.on('message', message => {
         message.channel.send('HUUU YEAH!');
     }
 });
-
-//Uno 2.0
-  
-//Uno
-
-client.on("message", async message => {
-    if (message.content.toLowerCase() === "!creategame")
-        await discordUNO.createGame(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase() === "!join")
-        await discordUNO.addUser(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase() === "!leave")
-        await discordUNO.removeUser(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase() === "!startgame")
-        await discordUNO.startGame(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase() === "!endgame")
-        await discordUNO.endGame(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase() === "!closegame")
-        await discordUNO.closeGame(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase().startsWith("!play"))
-        await discordUNO.playCard(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase().startsWith("!UNO"))
-        await discordUNO.UNO(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase() === "!draw")
-        await discordUNO.draw(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase() === "!cards")
-        await discordUNO.viewCards(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase() === "!table")
-        await discordUNO.viewTable(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase() === "!viewwinners")
-        await discordUNO.viewWinners(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase() === "!settings")
-        await discordUNO.updateSetting(message);
-});
-
-client.on("message", async message => {
-    if (message.content.toLowerCase() === "!viewsettings") 
-        await discordUNO.viewSettings(message);
-});
-
-
 
 client.login(process.env.DJS_TOKEN)
