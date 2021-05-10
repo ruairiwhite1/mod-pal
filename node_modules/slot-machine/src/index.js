b22ce9598e72da4c64e3fd666cc14c9e2e98cc9a
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-05-10 12:46:38 +0100

    Refactoring

--- node_modules/slot-machine/src/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    Symbol: require('./struct/SlotSymbol'),
-    SlotSymbol: require('./struct/SlotSymbol'),
-    SlotMachine: require('./struct/SlotMachine'),
-    EvaluatedLine: require('./struct/EvaluatedLine'),
-    Results: require('./struct/Results')
-};
