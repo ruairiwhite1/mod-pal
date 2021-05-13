85ae1c5f61df670ff85a2a6fc5e6b7cb325b4032
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-05-13 20:33:57 +0100

    Added f command

---
+++ commands/commands/fun/f.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const { MessageEmbed, DiscordAPIError } = require("discord.js");
+
+module.exports = {
+    commands: "f",
+    description: "f command - inc. MessageEmbed",
+    callback: async (client, message, args) => {
+
+        if (!args[0]) {
+            message.delete().catch(() => {});
+            const embed = new Discord.MessageEmbed()
+             .setAuthor(`${message.author.username} has paid their respects`)
+             .setColor("RANDOM")
+            .setFooter('Press 🇫 to pay your respects') // You can change the emoji here to an custom one.
+            message.channel.send(embed).then(m => m.react("🇫")).catch(() => { });
+
+
+        }
+
+        else {
+            message.delete().cache(() => {
+                const embed = new Discord.MessageEmbed()
+                .setAuthor('\u2000', message.author.displayAvatarURL({ format: 'png' }))
+                .setColor('PURPLE')
+                 message.channel.send(embed).then(m => m.react("🇫")).catch(() => {});
+            })
+        }
+
+
+    }
+};
