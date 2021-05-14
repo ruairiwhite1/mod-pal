c03472660114367cbb1e1b172ccfc13d6aa01fcb
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-05-14 18:30:17 +0100

    Added custom prefixes support

---
+++ commands/commands/config/setprefix.js
@@ -0,0 +1,32 @@
+const mongo = require('@util/mongo')
+const commandPrefixSchema = require('@schemas/command-prefix-schemas')
+
+odule.exports = {
+    commands: "setprefix",
+    minArgs: 1,
+    maxArgs: 1,
+    expectedArgs: "<New Prefix>",
+    permissionError: "Sorry you must have the **ADMINISTRATOR** permission to run this command",
+    permissions: 'ADMINISTRATOR',
+    callback: async (message, arguments, text) => {
+        await mongo().then(async mongoose => {
+            try {
+                const guildId = message.guild.id
+                const prefix = arguments[0]
+
+                await commandPrefixSchema.findOneAndUpdate({
+                    _id: guildId
+                }, {
+                    _id: guildId,
+                    prefix: prefix
+                }, {
+                    upsert: true
+                })
+
+                message.reply(`Bot prefix has been updated to ${prefix}`)
+            } finally {
+                mongoose.connection.close
+            }
+        })
+    }
+}
