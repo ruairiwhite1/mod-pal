fccbfc261cb2055714dce4d02379d083a880be76
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-05-15 14:23:25 +0100

    Added bot and user info cmds

---
+++ commands/misc/botinfo.js
@@ -0,0 +1,49 @@
+const { MessageEmbed } = require('discord.js')
+const { version } = require('@root/package.json')
+
+module.exports = {
+      name: 'botinfo',
+      category: 'Information',
+      description: 'Displays bot information',
+      callback: async ({ message, args, text, client, prefix, instance }) => {
+        let totalMembers = 0
+    
+        for (const guild of this.client.guilds.cache) {
+          totalMembers += (await guild[1].members.fetch()).size
+        }
+    
+        const embed = new MessageEmbed()
+          .setAuthor(
+            `Information about the ${this.client.user.username} Bot`,
+            this.client.user.displayAvatarURL()
+          )
+          .addFields(
+            {
+              name: 'Bot tag',
+              value: this.client.user.tag,
+            },
+            {
+              name: 'Version',
+              value: version,
+            },
+            {
+              name: "Server's command prefix",
+              value: message.guild.commandPrefix,
+            },
+            {
+              name: 'Time since last restart',
+              value: `${process.uptime().toFixed(2)}s`,
+            },
+            {
+              name: 'Server count',
+              value: this.client.guilds.cache.size,
+            },
+            {
+              name: 'Total members',
+              value: totalMembers,
+            }
+          )
+    
+        message.channel.send(embed)
+      }
+    }
