2cac02c48add078f8f340e6670706a1aadbc1d55
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-05-18 16:06:07 +0100

    Update levels.js

--- features/levels.js
+++ features/levels.js
@@ -10,16 +10,15 @@ client.on("message", async message => {
   Levels.setURL(config.mongoPath)
   
     if (!message.guild) return;
+    if (message.author.bot) return;
     if (message.author.client) return;
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-
-    const randomXp = Math.floor(Math.random() * 9) + 1; //Random amont of XP until the number you want + 1
+    const randomXp = Math.floor(Math.random() * 9) + 1; 
     const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
     if (hasLeveledUp) {
         const user = await Levels.fetch(message.author.id, message.guild.id);
         message.channel.send(`You leveled up to ${user.level}! Keep it going!`);
     }
-
+}
+)
 }
