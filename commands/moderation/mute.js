a0054f401409ab1be6cb05aec91af73831ec14ca
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-05-24 20:11:29 +0100

    New mod cmds

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,55 +1,272 @@
-const { MessageCollector } = require('discord.js')
-const muteSchema = require('@schemas/mute-schema')
-
+const { MessageEmbed } = require('discord.js');
+const Guild = require("@schemas/guilds.js");
+const Economy = require("@features/economy.js")
+const mongoose = require("mongoose")
+const ms = require("ms")
+const muteModel = require("@util/mute.js")
+const Discord = require("discord.js");
+const Logging = require('@schemas/logging.js')
 module.exports = {
-    commands: "mute",
-    category: 'Moderation',
-    description: 'Mute a member for a certain amount of time',
-    requiredPermissions: ['KICK_MEMBERS'],
-    expectedArgs: '<@user> <time> <m/h/d/life> <reason>',
-    callback: async ({ message, args, text, client, prefix, instance }) => {
-        const durations = {
-            m: 60,
-            h: 60 * 60,
-            d: 60 * 60 * 24,
-            life: -1,
-          }
-        if (args.length !== 4) {
-            message.reply('Incorrect syntax! Please use !mute <@user> <time> <reason>')
-            return
-        }
-        
-        const target = message.mentions.users.first()
-        if (!target) {
-            message.reply('Please specify a user to mute!')
-            return
-        }
+        name: 'mute',
+        aliases: [ 'm','tempmute' ],
+        description: 'Mute the specified user from the guild',
+        category: 'Moderation',
+        expectedArgs: '<user> [time]',
+        permissions: ['MANAGE_ROLES'],
+        callback: async ({ message, args, text, client, prefix, instance }) => {
 
-        const time = args[1]
-        if (isNaN(time)) {
-            message.reply('Please specify a valid time!')
-            return
+            const settings = await Guild.findOne({
+                guildId: message.guild.id
+            }, (err, guild) => {
+                if (err) console.error(err)
+                if (!guild) {
+                    const newGuild = new Guild({
+                      _id: mongoose.Types.ObjectId(),
+                      guildId: message.guild.id,
+                      guildName: message.guild.name,
+                      prefix: client.config.prefix,
+                      language: "english"
+                    })
+    
+                    newGuild.save()
+                    .then(result => console.log(result))
+                    .catch(err => console.error(err));
+    
+                    return message.channel.send('This server was not in our database! We have added it, please retype this command.').then(m => m.delete({timeout: 10000}));
+                }
+            });
+        const logging = await Logging.findOne({ guildId: message.guild.id })
+      const guildDB = await Guild.findOne({
+        guildId: message.guild.id
+      });
+      const language = require(`@util/language/${guildDB.language}.json`)
+      
+         let mentionedMember = message.mentions.members.last() || message.guild.members.cache.get(args[0]);
+    
+        const msRegex = RegExp(/(\d+(s|m|h|w))/)
+        let muteRole = await message.guild.roles.cache.get(Logging.moderation.mute_role);
+    
+         if (!mentionedMember) {
+            return message.channel.send(new Discord.MessageEmbed()
+                .setDescription(`'☹️ | ${language.banUserValid}`)
+                .setColor(client.color.red))
         }
-
-        const durationType = args[2]
-        if (!durations[durationType]) {
-            message.reply('Please specify a valid time frame')
-            return
+        else if (!msRegex.test(args[1])) {
+            return message.channel.send(new Discord.MessageEmbed()
+                .setDescription(`'☹️ | ${language.muteTime}`)
+                .setColor(client.color.red))
         }
-
-        let reason = args[3]
-        if(!reason) reason = 'No provided'
-
-         const currentlyMuted = previousMutes.filter(mute => {
-            return mute.content === true
+    
+        if (!muteRole) {
+            muteRole = await message.guild.roles.create({
+                data: {
+                    name: 'Muted',
+                    color: 'BLACK',
+                }
+            }).catch(()=>{})
+    
+            logging.moderation.mute_role = muteRole.id;
+            await logging.save().catch(()=>{})
+        }
+    
+        if (mentionedMember.roles.highest.position >= message.guild.me.roles.highest.position) {
+            return message.channel.send(new Discord.MessageEmbed()
+                .setDescription(`'☹️ | ${language.muteRolePosition}`)
+                .setColor(client.color.red))
+        }
+        else if (muteRole.position >= message.guild.me.roles.highest.position) {
+            return message.channel.send(new Discord.MessageEmbed()
+                .setDescription(`'☹️ | ${language.muteRolePositionBot}`)
+                .setColor(client.color.red))
+        }
+    
+        const isMuted = await muteModel.findOne({
+            guildID: message.guild.id,
+            memberID: mentionedMember.user.id,
         })
-
-        if (currentlyMuted.length) {
-            message.reply('That user is already muted!')
-            return
+    
+        if (isMuted) {
+            return message.channel.send(new Discord.MessageEmbed()
+                .setDescription(`'☹️ | ${language.muteMuted}`)
+                .setColor(client.color.red))
         }
-
-        const expires = new Date()
-        expires.setHours(expires.get() + duration)
+    let reason = args.slice(1).join(' ');
+    if (!reason) reason = `${language.noReasonProvided}`;
+    if (reason.length > 1024) reason = reason.slice(0, 1021) + '...';
+          let dmEmbed;
+    if(logging && logging.moderation.mute_action && logging.moderation.mute_action !== "1"){
+    
+      if(logging.moderation.mute_action === "2"){
+    dmEmbed = `☹️ You've been muted in **${message.guild.name}** for **${msRegex.exec(args[1])[1]}**`
+      } else if(logging.moderation.mute_action === "3"){
+    dmEmbed = `☹️ You've been muted in **${message.guild.name} ** for **${msRegex.exec(args[1])[1]}**\n\n__**Reason:**__ ${reason}`
+      } else if(logging.moderation.mute_action === "4"){
+    dmEmbed = `☹️ You've been muted in **${message.guild.name}** for **${msRegex.exec(args[1])[1]}**\n\n__**Moderator:**__ ${message.author} **(${message.author.tag})**\n__**Reason:**__ ${reason}`
+      }
+    
+    mentionedMember.send(new MessageEmbed().setColor('RED')
+    .setDescription(dmEmbed)
+    ).catch(()=>{})
     }
-}
+        message.channel.send(new Discord.MessageEmbed().setColor('GREEN').setDescription(`🎉 | Muted **${mentionedMember.user.tag}** for **${msRegex.exec(args[1])[1]}** ${logging && logging.moderation.include_reason === "true" ?`\n\n**Reason:** ${reason}`:``}`)).then(async(s)=>{
+              if(logging && logging.moderation.delete_reply === "true"){
+                setTimeout(()=>{
+                s.delete().catch(()=>{})
+                }, 5000)
+              }
+            })
+            .catch(()=>{});
+    
+        for (const channel of message.guild.channels.cache) {
+            channel[1].updateOverwrite(muteRole, {
+                SEND_MESSAGES: false,
+                CONNECT: false,
+            }).catch(()=>{})
+        }
+    
+        const noEveryone = mentionedMember.roles.cache.filter(r => r.name !== '@everyone' && !r.managed && r.id !== muteRole.id) 
+    
+      let delaynumber = 750;
+      if(mentionedMember.roles.cache.size - 1 > 10) delaynumber = 3000;
+    
+        if(logging && logging.moderation.remove_roles === "true"){
+        for (const role of noEveryone) {
+      
+     await mentionedMember.roles.remove(role, [`Reason: Mute Command - ${reason} | Responsible User: ${message.author.tag}`]).catch(()=>{})
+     await delay(delaynumber);
+          
+           
+        }
+        }
+    
+        await mentionedMember.roles.add(muteRole.id, [`Mute Command - ${reason} / Responsible User: ${message.author.tag}`]).catch(()=>{})
+    
+        const muteDoc = new muteModel({
+            guildID: message.guild.id,
+            memberID: mentionedMember.user.id,
+            length: Date.now() + ms(msRegex.exec(args[1])[1]),
+            memberRoles: noEveryone.map(r => r)
+        })
+    
+        await muteDoc.save().catch(()=>{})
+     
+     
+        
+                if(logging){
+      if(logging.moderation.delete_after_executed === "true"){
+      message.delete().catch(()=>{})
+    }
+    
+    const role = message.guild.roles.cache.get(logging.moderation.ignore_role);
+    const channel = message.guild.channels.cache.get(logging.moderation.channel)
+    
+      if(logging.moderation.toggle == "true"){
+        if(channel){
+        if(message.channel.id !== logging.moderation.ignore_channel){
+          if(!role || role && !message.member.roles.cache.find(r => r.name.toLowerCase() === role.name)){
+    
+    if(logging.moderation.mute == "true"){
+      
+    let color = logging.moderation.color;
+    if(color == "#000000") color = 'RED';
+    
+    let logcase = logging.moderation.caseN
+    if(!logcase) logcase = `1`
+    
+    const logEmbed = new MessageEmbed()
+    .setAuthor(`Action: \`Mute\` | ${mentionedMember.user.tag} | Case #${logcase}`, mentionedMember.user.displayAvatarURL({ format: 'png' }))
+    .addField('User', mentionedMember, true)
+    .addField('Moderator', message.member, true)
+    .addField('Length',  msRegex.exec(args[1])[1], true)
+    .setFooter(`ID: ${mentionedMember.id}`)
+    .setTimestamp()
+    .setColor(color)
+    
+    channel.send(logEmbed).catch(()=>{})
+    
+    logging.moderation.caseN = logcase + 1
+    await logging.save().catch(()=>{})
+    }
+          }
+        }
+        }
+      }
+    }
+            
+            setTimeout(async () => {
+            const isMuted = await muteModel.findOne({
+            guildID: message.guild.id,
+            memberID: mentionedMember.user.id,
+        })
+        
+    
+            let muteRole = await message.guild.roles.cache.get(logging.moderation.mute_role);
+    
+      
+        await mentionedMember.roles.remove(muteRole.id, [`Mute Command / Responsible User: ${message.author.tag}`]).catch(()=>{})
+    
+    
+        if(logging && logging.moderation.remove_roles === "true"){
+        for (const role of isMuted.memberRoles) {
+     const roleM = await message.guild.roles.cache.get(role);
+    if(roleM){
+     await mentionedMember.roles.add(roleM, ["Mute Command, mute duration expired."]).catch(()=>{})
+     await delay(750);
+    }
+     
+        }
+        }
+    
+         await isMuted.deleteOne().catch(()=>{})
+          
+    
+    
+    
+    
+                        if(logging){
+      
+    const role = message.guild.roles.cache.get(logging.moderation.ignore_role);
+    const channel = message.guild.channels.cache.get(logging.moderation.channel)
+    
+      if(logging.moderation.toggle == "true"){
+        if(channel){
+        if(message.channel.id !== logging.moderation.ignore_channel){
+          if(!message.member.roles.cache.find(r => r.name.toLowerCase() === role.name)) {
+    
+    if(logging.moderation.mute == "true"){
+      
+    let color = logging.moderation.color;
+    if(color == "#000000") color = 'GREEN';
+    
+    let logcase = logging.moderation.caseN
+    if(!logcase) logcase = `1`
+    
+    const logEmbedm = new MessageEmbed()
+    .setAuthor(`Action: \`Un Mute\` | ${mentionedMember.user.tag} | Case #${logcase}`, mentionedMember.user.displayAvatarURL({ format: 'png' }))
+    .addField('User', mentionedMember, true)
+    .addField('Reason',  'Mute Duration Expired', true)
+    .setFooter(`ID: ${mentionedMember.id}`)
+    .setTimestamp()
+    .setColor(color)
+    
+    channel.send(logEmbedm).catch(()=>{})
+    
+    logging.moderation.caseN = logcase + 1
+    await logging.save().catch(()=>{})
+    }
+          }
+        }
+        }
+      }
+    }
+    
+            }, ms(msRegex.exec(args[1])[1]))
+        }
+    }
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    
+
+
+    
---
+++ commands/moderation/unlock.js
@@ -0,0 +1,150 @@
+const { MessageEmbed } = require('discord.js');
+const Guild = require("@schemas/guilds.js");
+const Economy = require("@features/economy.js")
+const mongoose = require("mongoose")
+const Logging = require('@schemas/logging.js')
+
+module.exports = {
+        name: 'unlock',
+        aliases: [ 'unlc' ],
+        description: 'unLocks the current / mentioned channel.',
+        category: 'Moderation',
+        expectedArgs: '<channel> [time]',
+        permissions: ['MANAGE_CHANNELS'],
+        callback: async ({ message, args, text, client, prefix, instance }) => {
+            /*------ Guild Data ------*/
+            const fail = '☹️'
+            const success = '🎉'
+       
+       
+         const settings = await Guild.findOne({
+           guildId: message.guild.id
+       }, (err, guild) => {
+           if (err) console.error(err)
+           if (!guild) {
+               const newGuild = new Guild({
+                 _id: mongoose.Types.ObjectId(),
+                 guildId: message.guild.id,
+                 guildName: message.guild.name,
+                 prefix: client.config.prefix,
+                 language: "english"
+               })
+       
+               newGuild.save()
+               .then(result => console.log(result))
+               .catch(err => console.error(err));
+       
+               return message.channel.send('This server was not in our database! We have added it, please retype this command.').then(m => m.delete({timeout: 10000}));
+           }
+       });
+         const logging = await Logging.findOne({ guildId: message.guild.id })
+       const guildDB = await Guild.findOne({
+       guildId: message.guild.id
+       });
+       const language = require(`@util/language/${guildDB.language}.json`)
+       
+       
+       
+       
+               let channel = message.mentions.channels.first();
+               let reason = args.join(" ") || '`none`'
+       
+       let member =  message.guild.roles.cache.find(r => r.name.toLowerCase() === 'member');
+       let memberr =  message.guild.roles.cache.find(r => r.name.toLowerCase() === 'members');
+       let verified =  message.guild.roles.cache.find(r => r.name.toLowerCase() === 'verified');
+        if(channel) {
+                   reason = args.join(" ").slice(22) || '`none`'
+               } else (
+                   channel = message.channel
+               )
+       
+               if(channel.permissionsFor(message.guild.id).has('SEND_MESSAGES') === true) {
+                   const lockchannelError2 = new MessageEmbed()
+                   .setDescription(`${fail} | ${channel} is already unlocked`)
+                   .setColor('RED')
+       
+                   return message.channel.send(lockchannelError2)
+               }
+             
+               channel.updateOverwrite(message.guild.me, { SEND_MESSAGES: true }).catch(()=>{})
+            
+               channel.updateOverwrite(message.guild.id, { SEND_MESSAGES: true }).catch(()=>{})
+            
+               channel.updateOverwrite(message.author.id, { SEND_MESSAGES: true }).catch(()=>{})
+       
+               if(member){
+             channel.updateOverwrite(member, { SEND_MESSAGES: true }).catch(()=>{})
+               }
+               
+               if(memberr){
+         channel.updateOverwrite(memberr, { SEND_MESSAGES: true }).catch(()=>{})
+               }
+             
+             if(verified){
+        channel.updateOverwrite(verified, { SEND_MESSAGES: true }).catch(()=>{})
+       
+             }
+              
+       
+               const embed = new MessageEmbed()
+               .setDescription(`${success} | successfully Unlocked **${channel}** ${logging && logging.moderation.include_reason === "true" ?`\n\n**Reason:** ${reason}`:``}`)
+               .setColor('GREEN')
+               message.channel.send(embed).then(async(s)=>{
+                 if(logging && logging.moderation.delete_reply === "true"){
+                   setTimeout(()=>{
+                   s.delete().catch(()=>{})
+                   }, 5000)
+                 }
+               })
+               .catch(()=>{});
+       
+           
+       
+       
+       if(logging){
+       if(logging.moderation.delete_after_executed === "true"){
+         message.delete().catch(()=>{})
+       }
+       
+       const role = message.guild.roles.cache.get(logging.moderation.ignore_role);
+       const channel = message.guild.channels.cache.get(logging.moderation.channel)
+       
+         if(logging.moderation.toggle == "true"){
+           if(channel){
+             
+           if(message.channel.id !== logging.moderation.ignore_channel){
+         if(!role || role && !message.member.roles.cache.find(r => r.name.toLowerCase() === role.name)){
+       
+       if(logging.moderation.lock == "true"){
+         
+       let color = logging.moderation.color;
+       
+       let logcase = logging.moderation.caseN
+       if(!logcase) logcase = `1`
+       
+       let reason = args.slice(1).join(' ');
+       if (!reason) reason = `${language.noReasonProvided}`;
+       if (reason.length > 1024) reason = reason.slice(0, 1021) + '...';
+       
+       const logEmbed = new MessageEmbed()
+       .setAuthor(`Action: \`UnLock\` | ${message.author.tag} | Case #${logcase}`, message.author.displayAvatarURL({ format: 'png' }))
+       .addField('Channel', channel, true)
+       .addField('Moderator', message.member, true)
+       .addField('Reason', reason, true)
+       .setFooter(`ID: ${message.author.id}`)
+       .setTimestamp()
+       .setColor('GREEN')
+       
+       channel.send(logEmbed).catch(()=>{})
+       
+       logging.moderation.caseN = logcase + 1
+       await logging.save().catch(()=>{})
+       }
+             }
+           }
+           }
+         }
+       }
+      }
+
+}
