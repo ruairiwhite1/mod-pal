bf2bf0b7d3e09431c9ed45734783a8092481d9a6
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-06-04 19:28:27 +0100

    Update calcuator.js

--- commands/misc/calcuator.js
+++ commands/misc/calcuator.js
@@ -1,7 +1,7 @@
 module.exports = {
     commands: ['calculator', 'calc'],
     description: 'A simple calculator',
-    callback: ({ message, args, text, client, prefix, instance }) => {
+    callback: async ({ message, args, text, client, prefix, instance }) => {
         const { Calculator } = require('weky')
         await Calculator(message)
     }
