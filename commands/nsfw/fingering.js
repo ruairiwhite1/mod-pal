e934769c88ac12bb468559008b24dacbeb1863a1
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-07-22 21:02:37 +0100

    Create fingering.js

---
+++ commands/nsfw/fingering.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+        name: 'fingering',
+        description: 'Nice and good, up your bum',
+        category: 'NSFW',
+        callback: async ({ message, args, text, client, prefix, instance }) => {
+      try {
+        var subreddits = [
+          'Fingering',
+          'FingeringFever',
+          'fingering_babes',
+          'FingeringHerself'
+        ]
+
+        var reddit = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+      
+        const data = await fetch(`https://meme-api.herokuapp.com/gimme/${reddit}`).then(res => res.json())
+
+        if (!data) return message.channel.send(`Sorry, seems like i can't connect to API.`);
+      
+        const { title, postLink, url, subreddit } = data
+
+        message.channel.send({
+          embed: {
+            color: "BLURPLE",
+            title: `${title}`,
+            url: `${postLink}`,
+            image: {
+              url: url
+            },
+            footer: { text: `/reddit/${subreddit}` }
+          }
+        });
+      } catch(error) {
+        client.emit("apiError", error, message);
+      }
+    }
+}
