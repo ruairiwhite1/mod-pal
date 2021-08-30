0f746f69e1a607c76b0efa0d56fa2a33090415a3
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-08-30 10:38:26 +0100

    Fixed seek and skip commands/

--- commands/music/seek.js
+++ commands/music/seek.js
@@ -6,7 +6,6 @@ module.exports = {
     description: 'Seek to a certain part in the music',
     callback: async ({ message, args, text, client, prefix, instance }) => {
 
-        
         client.distube.seek(message, Number(args[0]))
         message.channel.send('Now seeking...')
     }
