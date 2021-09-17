7e8448d088b11737b4c5e7e71ed8656527ba60f3
ruairiwhite1 <52743238+ruairiwhite1@users.noreply.github.com>
2021-09-17 20:20:48 +0100

    New nsfw commands

---
+++ commands/nsfw/incest.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+        name: 'incest',
+        description: 'Sweet home Alabama',
+        category: 'NSFW',
+        callback: async ({ message, args, text, client, prefix, instance }) => {
+      try {
+        if(!message.channel.nsfw){ message.channel.send("This command can only be used in channels marked nsfw."); return; }
+        var subreddits = [
+          'incestporn',
+          'wincest',
+          'incest_gifs'
+        ]
+
+        var reddit = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+      
+        const data = await fetch(`https://meme-api.herokuapp.com/gimme/${reddit}`).then(res => res.json())
+
+        if (!data) return message.channel.send(`Sorry, seems like i can't connect to API.`);
+      
+        const { title, postLink, url, subreddit } = data
+
+        message.channel.send({
+          embed: {
+            color: "BLURPLE",
+            title: `${title}`,
+            url: `${postLink}`,
+            image: {
+              url: url
+            },
+            footer: { text: `/reddit/${subreddit}` }
+          }
+        });
+      } catch(error) {
+        client.emit("apiError", error, message);
+      }
+    }
+}
---
+++ commands/nsfw/overwatch.js
@@ -0,0 +1,38 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+        name: 'Overwatch',
+        description: 'Overwatch porn',
+        category: 'NSFW',
+        callback: async ({ message, args, text, client, prefix, instance }) => {
+      try {
+        if(!message.channel.nsfw){ message.channel.send("This command can only be used in channels marked nsfw."); return; }
+        var subreddits = [
+          'rule34overwatch',
+          'overwatch_porn'
+        ]
+
+        var reddit = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+      
+        const data = await fetch(`https://meme-api.herokuapp.com/gimme/${reddit}`).then(res => res.json())
+
+        if (!data) return message.channel.send(`Sorry, seems like i can't connect to API.`);
+      
+        const { title, postLink, url, subreddit } = data
+
+        message.channel.send({
+          embed: {
+            color: "BLURPLE",
+            title: `${title}`,
+            url: `${postLink}`,
+            image: {
+              url: url
+            },
+            footer: { text: `/reddit/${subreddit}` }
+          }
+        });
+      } catch(error) {
+        client.emit("apiError", error, message);
+      }
+    }
+}
